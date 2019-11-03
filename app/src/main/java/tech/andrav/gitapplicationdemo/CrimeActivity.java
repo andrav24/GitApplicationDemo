8e03abf66bb1307df0f8aac55fc6907c3569961f
andrav24 <andrav24@yandex.ru>
2019-11-03 23:50:04 +0700

    Add fragment

--- app/src/main/java/tech/andrav/gitapplicationdemo/CrimeActivity.java
+++ app/src/main/java/tech/andrav/gitapplicationdemo/CrimeActivity.java
@@ -1,6 +1,9 @@
 package tech.andrav.gitapplicationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
 
 public class CrimeActivity extends AppCompatActivity {
@@ -9,5 +12,15 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (null == fragment) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
